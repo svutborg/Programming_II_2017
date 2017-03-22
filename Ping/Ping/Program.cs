908d19ed302e7be65689d7a7eb9caade1a01ca44
Steffen Rahbek Vutborg <svu@ucn.dk>
2017-03-22 09:57:49 +0100

    module 2N

---
+++ Ping/Ping/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Ping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.NetworkInformation.Ping P = new System.Net.NetworkInformation.Ping();
+            Console.WriteLine("What to ping?");
+            string reply = Console.ReadLine();
+
+            P.PingCompleted += P_PingCompleted;
+            P.SendAsync(reply,null);
+            /*PingReply r = P.Send(reply);
+
+            Console.WriteLine(r);
+
+
+            */
+            Console.Read();
+            
+        }
+
+        private static void P_PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            Console.WriteLine(e.Reply.RoundtripTime);
+        }
+    }
+}
---
+++ ThreadDelegate/ThreadDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread T = new Thread(delegate() { ThreadMethod("Some message", 50000); });
+            T.Start();
+
+            Console.Read();
+
+        }
+
+        static int ThreadMethod(string mes, int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i + ": " + mes);
+            }
+            return 42;
+        }
+    }
+}
