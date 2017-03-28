9f437ed6bd38e2e9cf28a6f8f1b981d173764c0d
Steffen Rahbek Vutborg <svu@ucn.dk>
2017-03-28 11:03:02 +0200

    module 2E

---
+++ CrossThreadAccess/CrossThreadAccess/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CrossThreadAccess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //ThreadMethod();
+            Thread T = new Thread(delegate() { ThreadMethod(listBox1); });
+            T.Start();
+            T = new Thread(delegate () { ThreadMethod(listBox2); });
+            T.Start();
+        }
+
+        private void ThreadMethod(ListBox l)
+        {
+            while (true)
+            {
+                this.Invoke((MethodInvoker)delegate () { l.Items.Add("Hi!"); });
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ MoreThreadExamples/MoreThreadExamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO.Ports;
+
+namespace MoreThreadExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort com = new SerialPort("COM5");
+            string s = "ComPort opener thread";
+            Thread T = new Thread(
+                delegate () 
+                {
+                    ThreadMethod(s, com);
+                }
+            );
+            T.Start();
+
+            Console.Read();
+        }
+
+        static void ThreadMethod(string message, SerialPort port)
+        {
+            Console.WriteLine(message);
+            port.Open();
+            Console.WriteLine("Port now opened");
+            while (true)
+            {
+                if(port.BytesToRead>0)
+                    Console.WriteLine(port.ReadExisting());
+            }
+            
+        }
+    }
+}
